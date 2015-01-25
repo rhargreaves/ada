2fe6164d3259b597787c81fbe3eac6202295318d
Robert Hargreaves <kiyouta@gmail.com>
2015-01-25 15:42:21 +0000

    Added demo app

---
+++ src/Ada.Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Ada.Bass;
+using Ada.Interfaces;
+
+namespace Ada.Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Init BaseEngine
+            var engine = new BassEngine();
+            engine.Init();
+
+            // Create source
+            ISource source = engine.CreateSource(new Uri(Path.GetFullPath("A1.wav")));
+
+            // Add source to mixer
+            engine.Mixer.AddSource(source, false);
+
+            // Start mixer
+            engine.Mixer.Play();
+
+            Console.ReadKey();
+        }
+    }
+}
